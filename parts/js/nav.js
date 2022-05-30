5e605edcaa84997b0dab7e044755ec30b60ff33e
little-sheep <1223125595@qq.com>
2022-05-30 19:13:48 +0800

    add nav.html, nav.js and some styles about nav.html

---
+++ parts/js/nav.js
@@ -0,0 +1,6 @@
+const navbarBtn = document.getElementById("navbar-btn");
+const navbarLinks = document.getElementById("navbar-links");
+
+navbarBtn.addEventListener("click", () => {
+  navbarLinks.classList.add("show-navbar-list");
+});

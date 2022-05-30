5c9e2d921530c7162fc15b1de029d73000bdfc7a
little-sheep <1223125595@qq.com>
2022-05-30 23:12:54 +0800

    add some details about nav.html and its styles

--- parts/js/nav.js
+++ parts/js/nav.js
@@ -2,5 +2,6 @@ const navbarBtn = document.getElementById("navbar-btn");
 const navbarLinks = document.getElementById("navbar-links");
 
 navbarBtn.addEventListener("click", () => {
-  navbarLinks.classList.add("show-navbar-list");
+  // navbarLinks.classList.add("show-navbar-list");
+  navbarLinks.classList.toggle("show-navbar-list");
 });

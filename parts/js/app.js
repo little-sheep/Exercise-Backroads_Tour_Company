e6901c8ba524d7eb4b77d04c6b620a2377c17278
little-sheep <1223125595@qq.com>
2022-05-30 23:13:47 +0800

    add some js code about nav.html

--- parts/js/app.js
+++ parts/js/app.js
@@ -1,3 +1,11 @@
 const footerYear = document.getElementById("footer-year");
 
-footerYear.innerHTML=new Date().getFullYear();
+footerYear.innerHTML = new Date().getFullYear();
+
+
+const navbarBtn = document.getElementById("navbar-btn");
+const navbarLinks = document.getElementById("navbar-links");
+
+navbarBtn.addEventListener("click", () => {
+  navbarLinks.classList.toggle("show-navbar-list");
+});
